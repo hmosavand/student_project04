ea953b202246ff07a0ba6f53366db29933120e46
HamidMousavand <hmosavand@gmail.com>
2022-03-02 10:49:48 +0330

    Initial commit

---
+++ src/main/java/com/project/StudentProject04Application.java
@@ -0,0 +1,13 @@
+package com.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentProject04Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentProject04Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/student_project04/StudentProject04ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.student_project04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentProject04ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
